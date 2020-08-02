d89a3d7813ec6b7129234ff22faf7071cc1d93f8
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-08-02 15:53:39 +0530

    Update views.py

--- space/views.py
+++ space/views.py
@@ -5,3 +5,10 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView) :
     template_name = "index.html"
+
+
+class MenuView(TemplateView) :
+    template_name = "Menu/Home.html"
+
+class solsys(TemplateView) :
+    template_name = "Menu/SolarSystemSimulation.html"
