a3d1f8342b8611094a8d5f51c80c529f8d99a75c
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-08-02 17:29:36 +0530

    Update views.py

--- space/views.py
+++ space/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import TemplateView
+import requests 
 
 
 class HomeView(TemplateView) :
@@ -12,3 +13,20 @@ class MenuView(TemplateView) :
 
 class solsys(TemplateView) :
     template_name = "Menu/SolarSystemSimulation.html"
+
+
+def issview(request) :
+    url = "https://api.wheretheiss.at/v1/satellites/25544"
+    response = requests.get(url).json()
+    context = {
+        "name" : response["name"],
+        "id" : response["id"],
+        "lat" : response["latitude"],
+        "lon" : response["longitude"],
+        "alt" : response["altitude"],
+        "vel" : response["velocity"],
+        "time" : response["timestamp"],
+        "unit" : response["units"],
+    }
+
+    return(request,"iss.html",context)
